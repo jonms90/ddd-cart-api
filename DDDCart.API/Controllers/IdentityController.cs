45af524ed4cff4036623e206695f1b136c1b78f4
jonms90 <s231866@gmail.com>
2024-04-06 14:27:01 +0100

    add identitycontroller

---
+++ DDDCart.API/Controllers/IdentityController.cs
@@ -0,0 +1,48 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DDDCart.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public IdentityController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpPost("accessToken")]
+        public IActionResult GenerateToken()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new("userid", Guid.NewGuid().ToString())
+            };
+
+            var tokenDescription = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.Add(TimeSpan.FromHours(1)),
+                Issuer = _config["JwtSettings:Issuer"],
+                Audience = _config["JwtSettings:Audience"],
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"] ?? throw new ArgumentException())),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescription);
+
+            var jwt = tokenHandler.WriteToken(token);
+            return Ok(jwt);
+        }
+    }
+}
